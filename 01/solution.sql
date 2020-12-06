d3fa87f5361940a26dbf2f3e87634da7da8402ef
André Steenveld <andre@fusion-electronics.nl>
2020-12-06 01:23:59 +0100

    Fix input and do the second puzzl

--- 01/solution.sql
+++ 01/solution.sql
@@ -3,6 +3,8 @@ create table input ( value integer not null );
 .mode csv
 .import -v --csv --skip 1 '|/input' input
 
+.mode table
+
 select left.value as left
      , right.value as right
      , ( left.value + right.value ) as sum
@@ -10,3 +12,13 @@ select left.value as left
   from input as left
   cross join input as right
   where 2020 = ( left.value + right.value );
+
+select left.value as left
+     , center.value as center
+     , right.value as right
+     , ( left.value + center.value + right.value ) as sum
+     , ( left.value * center.value * right.value ) as multiplied
+  from input as left
+  cross join input as center
+  cross join input as right
+  where 2020 = ( left.value + center.value + right.value );
