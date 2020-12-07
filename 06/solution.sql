8cca03634656441231431a7657241efa843f438a
André Steenveld <andre@fusion-electronics.nl>
2020-12-07 17:52:04 +0100

    part two day six

--- 06/solution.sql
+++ 06/solution.sql
@@ -10,11 +10,42 @@ create table input (
 
 .mode table 
 
+--
+-- Part one
+--
 with 
     group_answers( group_no, answer, occurences ) as (
         select group_no, answer, count( * )
         from input
         group by group_no, answer
     )
--- First part of the puzzle
 select count( * ) from group_answers;
+
+--
+-- Part two
+--
+with
+    person ( group_no, person_no ) as (
+        select distinct group_no, person_no
+        from input
+    ),
+
+    group_size( group_no, size ) as (
+        select group_no, count( * )
+        from person
+        group by group_no
+    ),
+
+    group_answers( group_no, answer, occurences ) as (
+        select group_no, answer, count( * )
+        from input
+        group by group_no, answer
+    )
+select sum( s )
+from (
+    select count( group_no ) as s
+    from group_answers
+    join group_size using (group_no)
+    where occurences = size
+    group by group_no 
+);
